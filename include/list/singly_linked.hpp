4ac6fdc0d6a418e1880742258e7a12a70b25e3ea
Joshua O. <156972833+Jcokuad@users.noreply.github.com>
2026-03-29 20:21:42 -0400

    Updated singly linked list

--- include/list/singly_linked.hpp
+++ include/list/singly_linked.hpp
@@ -13,23 +13,59 @@ class SinglyLinkedList {
                 T elem;
                 Node* next;
                 // ToDo
-                Node(const T& element, Node* nxt = nullptr);
+                Node(const T& element, Node* nxt = nullptr)
+                : elem{element}, next{nxt} {}
         };
         int sz{0};
         Node* head{nullptr};
         Node* tail{nullptr};
     public:
         // ToDo
-        SinglyLinkedList();
-        int size() const;
-        bool empty() const;
-        T& front();
-        const T& front() const;
-        T& back();
-        const T& back() const;
-        void push_front(const T& elem);
-        void pop_front();
-        void push_back(const T& elem);
+        SinglyLinkedList() = default; // Constructs an empty list
+        int size() const {
+            return sz;
+        }
+        bool empty() const {
+            return sz == 0;
+        }
+        T& front() {
+            return head->elem; // Returns first element at head index
+        }
+        const T& front() const {
+            return head->elem;
+        }
+        T& back() {
+            return tail->elem; // return last element at tail index
+        }
+        const T& back() const {
+            return tail->elem;
+        }
+        void push_front(const T& elem) {
+            head = new Node(elem, head);  // assign new elem as head then increase sz
+            if (sz == 0) {
+                tail = head;
+            }
+            sz++;
+        }
+        void pop_front() {
+            Node* old{head}; // old is the current head of the list
+            head = head->next;
+            delete old; 
+            sz--; // assign second element as new head then delete first element and reduce size
+            if (sz == 0) {
+                tail = nullptr;
+            }
+        }
+        void push_back(const T& elem) {
+            Node* newest{new Node(elem)}; // assign elem to next index after the tail
+            if (sz == 0) {
+                head = newest;
+            } else {
+                tail->next = newest; // assign elem as new tail
+            }
+            tail = newest;
+            sz++;
+        }
         void concatenate(SinglyLinkedList& M);
         void reverse();
     public:
