68142e820c9958e007eb3131b48b144277add640
Joshua O. <156972833+Jcokuad@users.noreply.github.com>
2026-03-30 11:00:08 -0400

    Updated circularly linked header

--- include/list/circularly_linked.hpp
+++ include/list/circularly_linked.hpp
@@ -13,37 +13,84 @@ class CircularlyLinkedList {
                 T elem;
                 Node* next;
                 // ToDo
-                Node(const T& element, Node* nxt = nullptr);
+                Node(const T& element, Node* nxt = nullptr)
+                : elem{element}, next{nxt} {}
         };
     private:
         int sz{0};
         Node* tail{nullptr};
     public:
         // ToDo
-        CircularlyLinkedList();
-        int size() const;
-        bool empty() const;
-        T& front();
-        const T& front() const;
-        T& back();
-        const T& back() const;
-        void push_front(const T& elem);
-        void push_back(const T& elem);
-        void pop_front();
-        void rotate();
+        CircularlyLinkedList() = default; // constructs an empty list
+        int size() const {
+            return sz;
+        }
+        bool empty() const {
+            return sz == 0;
+        }
+        T& front() {
+            return head->elem; // Returns first element at head index
+        }
+        const T& front() const {
+            return head->elem;
+        }
+        T& back() {
+            return tail->elem; // return last element at tail index
+        }
+        const T& back() const {
+            return tail->elem;
+        }
+        void push_front(const T& elem) {
+            if (sz == 0) {
+                tail = new Node(elem);
+                tail->next = tail;
+            } else {
+                tail->next = new Node(elem, tail->next);
+            }
+            sz++;
+        }
+        void push_back(const T& elem) {
+            push_front(elem);
+            tail = tail->next;
+        }
+        void pop_front() {
+            Node* head = tail->next;
+            if (head == tail) { // if size == 1
+                tail = nullptr;
+            } else {  // size > 1
+                tail->next = head->next;
+            }
+            delete head; 
+            sz--;
+        }
+        void rotate() {   // rotates first element to the back of the list 
+            if (tail != nullptr) {
+                tail = tail->next;
+            }
+        }
 
         // Split *this into two circular lists A and B, each with half the nodes.
         // Precondition: sz is even. After the call, *this becomes empty and
         // A and B each point to one of the two halves.
         void splitEven(CircularlyLinkedList& A, CircularlyLinkedList& B) {
             // If the list is empty, both result lists should also be empty
-
+            if (empty()) {
+                return;
+            }
             // splitEven is only valid for even-sized lists else throw exception
-
+            if (sz % 2 != 0) {
+                throw std::runtime_error("splitEven only valid for even-sized lists");
+            }
+            int half = sz / 2;
             // head is the first node in the circular list
+            Node* head = tail->next;
 
             // Find the last node of the first half
-
+            Node* tail_first = head;
+            for (int i = 1; i < half; i++) {
+                tail_first = tail_first->next;
+            }
+            
             // The node after firstTail is the head of the second half
 
             // The original tail becomes the tail of the second half
@@ -56,7 +103,7 @@ class CircularlyLinkedList {
 
             // Original list becomes empty
         }
-        
+
     private:
         void clone(const CircularlyLinkedList& other);
     public:
