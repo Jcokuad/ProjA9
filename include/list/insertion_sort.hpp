0a41fc3ad241d02d235c7d7c4e88ccc10d4874a0
Joshua O. <156972833+Jcokuad@users.noreply.github.com>
2026-03-30 17:21:39 -0400

    Updated insertion_sort header

--- include/list/insertion_sort.hpp
+++ include/list/insertion_sort.hpp
@@ -5,8 +5,23 @@
 namespace dsac::list {
 
 template <typename List>
-void insertion_sort(List& data) {
-    
+void insertion_sort(List& data) {  //linked list insertion sort
+    if (data.size() > 1) {   //else no need to sort 
+        auto boundary{--data.end()};//last element 
+        while (boundary != data.begin()) { 
+            auto newest{boundary}; 
+            --newest;// before boundary 
+            auto walk{boundary}; //look entry >= newest (or end) 
+            while (walk != data.end() && *walk < *newest) 
+                ++walk; 
+            if (walk == boundary) { // newest is correct 
+                --boundary; // include in boundary 
+            } else { //re-insert newest element before walk 
+                data.insert(walk, *newest); 
+                data.erase(newest); // remove from original position 
+            } 
+        } 
+    }
 }
 
 }  // namespace dsac::list
